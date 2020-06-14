cdbef4f54be509ef3af92f172fa7c86ff0ca65fd
Jason Enstedt <jason_enstedt@hotmail.com>
2020-06-14 16:43:53 -0700

    fixed cors issue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {origins: '*:*'});
 
 
 // io.adapter(redisAdapter({ host: 'localhost', port: 6379 }));
